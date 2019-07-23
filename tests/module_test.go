e18314f7bc6ff39218619736fc9e8c45d625faa6
Gregory O <45366504+confusdcodr@users.noreply.github.com>
2019-07-23 08:34:49 -0400

    Apply suggestions from code review
    Co-Authored-By: Loren Gordon <lorengordon@users.noreply.github.com>

--- tests/module_test.go
+++ tests/module_test.go
@@ -26,16 +26,17 @@ func TestModule(t *testing.T) {
 
 func investigateDirectory(t *testing.T, directory os.FileInfo) {
 	// check if a prereq directory exists
-	if _, err := os.Stat(directory.Name() + "/prereq/"); err == nil {
-		prereqDir := directory.Name() + "/prereq/"
+	prereqDir := directory.Name() + "/prereq/"
+	if _, err := os.Stat(prereqDir); err == nil {
 		prereqOptions := createTerraformOptions(prereqDir)
 		defer terraform.Destroy(t, prereqOptions)
-		runTerraform(t, prereqOptions, false)
+		terraform.InitAndApply(t, prereqOptions)
 	}
 
 	// run terraform code for test case
 	terraformOptions := createTerraformOptions(directory.Name())
-	runTerraform(t, terraformOptions, true)
+	defer terraform.Destroy(t, terraformOptions)
+	terraform.InitAndApply(t, terraformOptions)
 }
 
 func createTerraformOptions(directory string) *terraform.Options {
@@ -45,14 +46,4 @@ func createTerraformOptions(directory string) *terraform.Options {
 	}
 
 	return terraformOptions
-}
-
-func runTerraform(t *testing.T, options *terraform.Options, destroyInfra bool) {
-	// At the end of the test, run `terraform destroy` to clean up any resources that were created
-	if destroyInfra {
-		defer terraform.Destroy(t, options)
-	}
-
-	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
-	terraform.InitAndApply(t, options)
 }
